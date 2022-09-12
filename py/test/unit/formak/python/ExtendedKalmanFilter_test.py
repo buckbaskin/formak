4f11cd176b4e2399647a68b7bcc629eeee851b75
Buck Baskin <mobile.wbaskin@gmail.com>
2022-09-11 20:39:00 -0700

    Add property based testing for the EKF process model

--- py/test/unit/formak/python/ExtendedKalmanFilter_test.py
+++ py/test/unit/formak/python/ExtendedKalmanFilter_test.py
@@ -1,7 +1,14 @@
 import numpy as np
+import warnings
 
+from scipy.stats import multivariate_normal
+from hypothesis import given, reject
+from hypothesis.strategies import floats
 from numpy.testing import assert_almost_equal
+
 from formak import ui, python
+
+warnings.filterwarnings("error")
 
 
 def test_EKF_process_with_control():
@@ -143,3 +150,76 @@ def test_EKF_process_jacobian():
         ekf.process_jacobian(dt=dt, state=state_vector, control=control_vector),
         [[1.0, 1.0], [0.0, 1.0]],
     )
+
+
+@given(floats(), floats(), floats())
+def test_EKF_impl_property(x, y, a):
+    config = {}
+    dt = 0.1
+
+    ui_Model = ui.Model(
+        ui.Symbol("dt"),
+        set(ui.symbols(["x", "y"])),
+        set(ui.symbols(["a"])),
+        {
+            ui.Symbol("x"): ui.Symbol("x") * ui.Symbol("y") + ui.Symbol("x"),
+            ui.Symbol("y"): "y + a * dt",
+        },
+    )
+    ekf = python.compile_ekf(
+        state_model=ui_Model,
+        process_noise=np.eye(1),
+        sensor_models={},
+        sensor_noises={},
+        config=config,
+    )
+
+    control_vector = np.array([[0.2]])
+    covariance = np.eye(2)
+    state_vector = np.array([[0.0, 0.0]]).transpose()
+
+    if not np.isfinite(state_vector).all() or not np.isfinite(control_vector).all():
+        reject()
+    if (np.abs(state_vector) > 1e100).any() or (np.abs(control_vector) > 1e100).any():
+        reject()
+
+    next_state, next_cov = ekf.process_model(
+        dt=dt, state=state_vector, covariance=covariance, control=control_vector
+    )
+
+    for i, key in enumerate(ekf.state_model.arglist[1 : ekf.state_size + 1]):
+        python_version = next_state[i]
+
+        subs_args = [
+            (symbol, float(val))
+            for (symbol, val) in zip(
+                ekf.state_model.arglist,
+                [dt] + list(state_vector) + list(control_vector),
+            )
+        ]
+        symbolic_version = ui_Model.state_model[key].subs(subs_args)
+
+        assert_almost_equal(python_version, symbolic_version)
+
+    try:
+        starting_central_probability = multivariate_normal(cov=covariance).pdf(
+            np.zeros_like(state_vector).transpose()
+        )
+        ending_central_probability = multivariate_normal(cov=next_cov).pdf(
+            np.zeros_like(state_vector).transpose()
+        )
+    except np.linalg.LinAlgError:
+        print("starting cov")
+        print(covariance)
+        print("next_cov")
+        print(next_cov)
+        raise
+
+    try:
+        assert ending_central_probability < starting_central_probability
+    except AssertionError:
+        print("starting pdf")
+        print(starting_central_probability)
+        print("ending pdf")
+        print(ending_central_probability)
+        raise
