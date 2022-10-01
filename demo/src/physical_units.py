a8e256b44e0cd6cc35779ce903d46173037db9eb
Buck Baskin <mobile.wbaskin@gmail.com>
2022-10-01 13:34:53 -0700

    Something like a final prototype for today

--- demo/src/physical_units.py
+++ demo/src/physical_units.py
@@ -3,63 +3,98 @@ from __future__ import annotations
 from typing import TypeVar, Generic, Tuple
 from types import GenericAlias
 
-Meter = TypeVar('Meter')
-Second = TypeVar('Second')
+Meter = TypeVar("Meter")
+Second = TypeVar("Second")
+
 
 def make_unit_subtype(physics_unit: str, quantity: int):
-    return TypeVar('%s^%d' % (physics_unit, quantity,))
+    return TypeVar(
+        "%s^%d"
+        % (
+            physics_unit,
+            quantity,
+        )
+    )
 
-class UnitImpl():
+
+class UnitImpl:
     def __repr__(self):
-        return 'Unit[%d,%d](%s)' % (self.meters, self.seconds, self.name,)
-
+        return "Unit[%d,%d](%s)" % (
+            self.meters,
+            self.seconds,
+            self.name,
+        )
 
     def __init__(self, name) -> None:
         self.name = name
 
     def __add__(self, rhs):
-        if (self.meters != rhs.meters):
-            raise ValueError('Mismatch in Meters: %d != %d' % (self.meters, rhs.meters,))
-        if (self.seconds != rhs.seconds):
-            raise ValueError('Mismatch in Seconds: %d != %d' % (self.seconds, rhs.seconds,))
+        if self.meters != rhs.meters:
+            raise ValueError(
+                "Mismatch in Meters: %d != %d"
+                % (
+                    self.meters,
+                    rhs.meters,
+                )
+            )
+        if self.seconds != rhs.seconds:
+            raise ValueError(
+                "Mismatch in Seconds: %d != %d"
+                % (
+                    self.seconds,
+                    rhs.seconds,
+                )
+            )
 
-        return Unit[self.meters, self.seconds]('%s+%s' % (self.name, rhs.name))
+        return Unit[self.meters, self.seconds]("%s+%s" % (self.name, rhs.name))
+
     def __mul__(self, rhs):
-        return Unit[self.meters + rhs.meters, self.seconds + rhs.seconds]('%s*%s' % (self.name, rhs.name))
+        return Unit[self.meters + rhs.meters, self.seconds + rhs.seconds](
+            "%s*%s" % (self.name, rhs.name)
+        )
+
     def __truediv__(self, rhs):
         try:
-            return Unit[self.meters - rhs.meters, self.seconds - rhs.seconds]('%s*%s' % (self.name, rhs.name))
+            return Unit[self.meters - rhs.meters, self.seconds - rhs.seconds](
+                "%s*%s" % (self.name, rhs.name)
+            )
         except AttributeError:
             assert not isinstance(rhs, Unit)
-            return Unit[self.meters, self.seconds]('%s*%s' % (self.name, rhs))
+            return Unit[self.meters, self.seconds]("%s*%s" % (self.name, rhs))
+
 
 class UnitMeta(type):
     def __getitem__(cls, key: Tuple[int, int]):
         meters, seconds = key
         env = dict(UnitImpl.__dict__)
-        env['meters'] = meters
-        env['seconds'] = seconds
-        
-        Type = type('Unit', (object,), env)
+        env["meters"] = meters
+        env["seconds"] = seconds
+
+        Type = type("Unit", (UnitImpl,), env)
         return GenericAlias(Type, key)
 
-class Unit(metaclass=UnitMeta):
-    pass
 
-dt = Unit[0,1]('dt') # Unit[0,1]
-accel = Unit[1,-2]('accel') # Unit[1,-2]
-jerk = Unit[1, -3]('jerk') # Unit[1,-3]
+class Unit(UnitImpl, metaclass=UnitMeta):
+    def __add__(self, rhs: Unit):
+        return super().__add__(rhs)
 
-print('a', accel)
-print('dt', dt)
-print('a * dt', accel * dt)
+
+dt = Unit[0, 1]("dt")  # Unit[0,1]
+accel = Unit[1, -2]("accel")  # Unit[1,-2]
+jerk = Unit[1, -3]("jerk")  # Unit[1,-3]
+
+print("a", accel)
+print("dt", dt)
+print("a * dt", accel * dt)
 
 vel = accel * dt + jerk * (dt * dt) / 2
-print('vel', vel)
+print("vel", vel)
 
-position = Unit[1,0]('position') # Unit[1,0]
+position = Unit[1, 0]("position")  # Unit[1,0]
 
-position = vel + position # Expect to fail here (velocity not the same units as position)
-print('position', position)
+position = (
+    vel + position
+)  # Expect to fail here (velocity not the same units as position)
+print("position", position)
 assert position.meters == 1
 assert position.seconds == 0
