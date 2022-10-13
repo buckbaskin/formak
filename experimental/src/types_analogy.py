cf536b1950d8475c5701c7207f8facb132935ab8
Buck Baskin <mobile.wbaskin@gmail.com>
2022-10-13 10:31:27 +0200

    Units as tuples example

--- experimental/src/types_analogy.py
+++ experimental/src/types_analogy.py
@@ -1,18 +1,83 @@
-# Meters, Seconds, Kilograms
-Unit = Tuple[int, int, int]
+from __future__ import annotations
+from typing import Tuple, Union
 
-Meters = (1,0,0)
-Seconds = (0,1,0)
-Kilograms = (0,0,1)
+# Meters, Seconds, Kilograms
+BaseUnit = Tuple[int, int, int]
+
+
+class UnaryOp(object):
+    def __init__(self, left: Unit) -> None:
+        self.left = left
+
+
+class BinOp(object):
+    def __init__(self, left: Unit, right: Unit) -> None:
+        self.left = left
+        self.right = right
+
+
+Unit = Union[BaseUnit, UnaryOp, BinOp]
+
+Radians = (0, 0, 0)
+Meters = (1, 0, 0)
+Seconds = (0, 1, 0)
+Kilograms = (0, 0, 1)
 MetersPerSecond = (1, -1, 0)
 MetersPerSecondSquared = (1, -2, 0)
 
-def mul(left, right):
-    def mul_impl():
-        for ul, ur in zip(left, right):
-            yield ul, ur
 
-    return tuple(mul_impl(left, right))
+def collapse(unit: Unit) -> BaseUnit:
+    # if isinstance(unit, BaseUnit):
+    if isinstance(unit, tuple) and len(unit) == 3:
+        return unit
+    if isinstance(unit, UnaryOp):
+        return collapse(unit.left)
+    if isinstance(unit, BinOp):
+        return tuple(
+            (ul + ur for ul, ur in zip(collapse(unit.left), collapse(unit.right)))
+        )
+
+
+def neg(left: Unit) -> UnaryOp:
+    return UnaryOp(left)
+
+
+def mul(left: Unit, right: Unit) -> BinOp:
+    return BinOp(left, right)
+
+
+def sin(angle: Radians) -> Radians:
+    assert collapse(angle) == Radians
+    return angle
+
+
+def add(left: Unit, right: Unit) -> BaseUnit:
+    assert collapse(left) == collapse(right)
+    return collapse(left)
+
+
+def eq(left: Unit, right: Unit) -> None:
+    assert collapse(left) == collapse(right)
+
+
+### Test Cases ###
 
 # F = ma
-Newtons = mul(Kilograms, MetersPerSecondSquared)
+Newtons = collapse(mul(Kilograms, MetersPerSecondSquared))
+assert Newtons == (1, -2, 1)
+
+try:
+    sin(Meters)
+    raise ValueError("Expected AssertionError")
+except AssertionError:
+    pass
+
+try:
+    add(Meters, Kilograms)
+    raise ValueError("Expected AssertionError")
+except AssertionError:
+    pass
+
+eq((1, 0, 1), mul(Meters, neg(Kilograms)))
+
+print("Done, Passing")
