fc18dcee526733485c2c3a94f2f8be1c57b97415
Buck Baskin <mobile.wbaskin@gmail.com>
2023-02-08 22:24:44 -0800

    Define some sort of (likely incorrect) compile_ekf

--- py/formak/cpp.py
+++ py/formak/cpp.py
@@ -194,3 +194,66 @@ def compile(symbolic_model, *, config=None):
     return CppCompileResult(
         success=True, header_path=args.header, source_path=args.source
     )
+
+def compile_ekf(
+    state_model, process_noise, sensor_models, sensor_noises, *, config=None
+):
+    if config is None:
+        config = Config()
+    elif isinstance(config, dict):
+        config = Config(**config)
+
+    parser = argparse.ArgumentParser(prog="generator.py")
+    parser.add_argument("--templates")
+    parser.add_argument("--header")
+    parser.add_argument("--source")
+
+    args = parser.parse_args()
+
+    header_template, source_template = args.templates.split(" ")
+
+    templates_base_path = dirname(header_template)
+    assert templates_base_path == dirname(source_template)
+
+    env = Environment(
+        loader=FileSystemLoader(templates_base_path), autoescape=select_autoescape()
+    )
+
+    try:
+        header_template = env.get_template("formak_model.h")
+        source_template = env.get_template("formak_model.cpp")
+    except TemplateNotFound:
+        print("Debugging TemplateNotFound")
+        print("Trying to scandir")
+        with scandir(templates_base_path) as it:
+            if len(list(it)) == 0:
+                print("No Paths in scandir")
+                raise
+
+        print("Walking")
+        for root, _, files in walk(templates_base_path):
+            depth = len(root.split("/"))
+            print("{}Root: {!s}".format(" " * depth, root))
+            for filename in files:
+                print("{}  - {!s}".format(" " * depth, filename))
+        print("End Walk")
+        raise
+
+    inserts = _generate_ekf_function_bodies(args.header, symbolic_model, config)
+
+    header_str = header_template.render(**inserts)
+    source_str = source_template.render(**inserts)
+
+    with open(args.header, "w") as header_file:
+        with open(args.source, "w") as source_file:
+            # Stack indents so an error in either file will write/close both the same way
+
+            print("Writing header arg {}".format(args.header))
+            header_file.write(header_str)
+
+            print("Writing source arg {}".format(args.source))
+            source_file.write(source_str)
+
+    return CppCompileResult(
+        success=True, header_path=args.header, source_path=args.source
+    )
