Fix two tests, improve the covariance test

py/test/unit/formak/cpp/generator_ekf.py
@@ -9,7 +9,7 @@ cpp_implementation = cpp.compile_ekf(
         set(ui.symbols(["a"])),
         {ui.Symbol("x"): "x * y", ui.Symbol("y"): "y + a * dt"},
     ),
-    process_noise=np.eye(1),
+    process_noise=np.eye(2) * 0.5,
     sensor_models={
         "simple": {"reading1": ui.Symbol("x")},
         "combined": {"reading2": ui.Symbol("x") + ui.Symbol("y")},