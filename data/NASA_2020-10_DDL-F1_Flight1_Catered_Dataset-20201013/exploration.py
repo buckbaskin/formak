1611f313d34d3c454a9ee977b82a8e3ff3dd136a
Buck Baskin <mobile.wbaskin@gmail.com>
2023-03-23 08:41:17 -0700

    IMU acceleration/delta V plotting

--- data/NASA_2020-10_DDL-F1_Flight1_Catered_Dataset-20201013/exploration.py
+++ data/NASA_2020-10_DDL-F1_Flight1_Catered_Dataset-20201013/exploration.py
@@ -3,31 +3,52 @@ Source page: https://data.nasa.gov/Aerospace/Deorbit-Descent-and-Landing-Flight-
 Download URL: https://data.nasa.gov/download/vicw-ivgd/application%2Fx-zip-compressed
 MD5Sum of data: be645485bfdbdc0edf184bd85d355e37
 """
+from math import sqrt
 from matplotlib import pyplot as plt
 import pandas as pd
 
+REFERENCE_TIME = 1602596210210000000
+
 lidar_data = pd.read_csv("Data/commercial_lidar.csv")
 lidar_data.columns = lidar_data.columns.str.strip()
-
-print("Original")
-print(lidar_data.keys())
-print(lidar_data)
-
-
 lidar_data["TIME_NANOSECONDS_TAI"] = lidar_data["TIME_NANOSECONDS_TAI"].apply(
-    lambda x: int(x)
+    lambda x: int(x) - REFERENCE_TIME
 )
 
-print("Parsed")
-print(lidar_data)
+imu_data = pd.read_csv("Data/dlc.csv")
+imu_data.columns = imu_data.columns.str.strip()
+imu_data["TIME_NANOSECONDS_TAI"] = imu_data["TIME_NANOSECONDS_TAI"].apply(
+    lambda x: int(x) - REFERENCE_TIME
+)
 
-for i in range(0, 4):
+imu_x = imu_data["DATA_DELTA_VEL[1]"]
+imu_y = imu_data["DATA_DELTA_VEL[2]"]
+imu_z = imu_data["DATA_DELTA_VEL[3]"]
+total_imu = imu_x * imu_x + imu_y * imu_y + imu_z * imu_z
+total_imu = total_imu.apply(sqrt)
+
+print("Lidar")
+print(lidar_data.keys())
+
+print("IMU")
+print(imu_data.keys())
+print(imu_data)
+
+UPDATE_RATE_HZ = 50
+
+for i in range(0, 3):
     plt.plot(
-        lidar_data["TIME_NANOSECONDS_TAI"] * 1e-9,
-        lidar_data["OMPS_DopplerSpeed_MpS[%d]" % (i + 1)],
-        label="Range[%d]" % (i + 1),
+        imu_data["TIME_NANOSECONDS_TAI"] * 1e-9,
+        imu_data["DATA_DELTA_VEL[%d]" % (i + 1)] * UPDATE_RATE_HZ,
+        label="%d" % (i + 1),
     )
-plt.xlabel('seconds')
-plt.ylabel('meters per second')
+
+plt.plot(
+    imu_data["TIME_NANOSECONDS_TAI"] * 1e-9,
+    total_imu * UPDATE_RATE_HZ,
+    label="total magnitude delta v",
+)
+plt.xlabel("seconds")
+plt.ylabel("meters / sec2 (approx, from delta v)")
 plt.legend()
 plt.show()
