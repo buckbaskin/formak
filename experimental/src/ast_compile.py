c7415474ca70c06df3b05d9cd8958fb07be78b7d
Buck Baskin <mobile.wbaskin@gmail.com>
2023-05-07 00:50:24 -0700

    State V1

--- experimental/src/ast_compile.py
+++ experimental/src/ast_compile.py
@@ -1,4 +1,5 @@
 import ast
+from itertools import chain
 from typing import List, Any, Optional
 from dataclasses import dataclass
 
@@ -368,7 +369,7 @@ class BaseAst(ast.AST):
         self.end_lineno = None
         self.end_col_offset = None
 
-    def compile(self, options: CompileState):
+    def compile(self, options: CompileState, **kwargs):
         raise NotImplementedError()
 
     def indent(self, options: CompileState):
@@ -376,8 +377,8 @@ class BaseAst(ast.AST):
 
 
 def autoindent(compile_func):
-    def wrapped(self, options: CompileState):
-        for line in compile_func(self, options):
+    def wrapped(self, options: CompileState, **kwargs):
+        for line in compile_func(self, options, **kwargs):
             yield " " * options.indent + line
 
     # TODO(buck): wrapper helper function
@@ -392,11 +393,11 @@ class Namespace(BaseAst):
     name: str
     body: List[Any]
 
-    def compile(self, options: CompileState):
+    def compile(self, options: CompileState, **kwargs):
         yield f"namespace {self.name} {{"
 
         for component in self.body:
-            yield from component.compile(options)
+            yield from component.compile(options, **kwargs)
 
         yield f"}} // namespace {self.name}"
 
@@ -410,7 +411,7 @@ class HeaderFile(BaseAst):
     includes: List[str]
     namespaces: List[Namespace]
 
-    def compile(self, options: CompileState):
+    def compile(self, options: CompileState, **kwargs):
         if self.pragma:
             yield "#pragma once"
             yield ""
@@ -420,7 +421,7 @@ class HeaderFile(BaseAst):
         yield ""
 
         for namespace in self.namespaces:
-            yield from namespace.compile(options)
+            yield from namespace.compile(options, **kwargs)
 
 
 @dataclass
@@ -434,7 +435,7 @@ class ClassDef(BaseAst):
     body: List[Any]
 
     @autoindent
-    def compile(self, options: CompileState):
+    def compile(self, options: CompileState, **kwargs):
         bases_str = ""
         if len(self.bases) > 0:
             raise NotImplementedError()
@@ -442,7 +443,7 @@ class ClassDef(BaseAst):
         yield f"{self.tag} {self.name} {bases_str} {{"
 
         for component in self.body:
-            yield from component.compile(options)
+            yield from component.compile(options, classname=self.name, **kwargs)
 
         yield "}"
 
@@ -456,11 +457,87 @@ class MemberDeclaration(BaseAst):
     value: Optional[Any] = None
 
     @autoindent
-    def compile(self, options: CompileState):
+    def compile(self, options: CompileState, **kwargs):
         value_str = ""
         if self.value is not None:
             value_str = f"= {self.value}"
         yield f"{self.type_} {self.name} {value_str};"
+
+
+@dataclass
+class UsingDeclaration(BaseAst):
+    _fields = ("name", "type_")
+
+    name: str
+    type_: str
+
+    @autoindent
+    def compile(self, options: CompileState, **kwargs):
+        yield f"using {self.name} = {self.type_};"
+
+
+class ConstructorDeclaration(BaseAst):
+    _fields = ("args",)
+
+    def __init__(self, *args):
+        self.args = args
+
+    @autoindent
+    def compile(self, options: CompileState, classname: str, **kwargs):
+        yield f"{classname}("
+        for arg in self.args:
+            for line in arg.compile(options, classname=classname, **kwargs):
+                yield line + ","
+        yield ");"
+
+
+@dataclass
+class Arg(BaseAst):
+    _fields = ("type_", "name")
+
+    type_: str
+    name: str
+
+    @autoindent
+    def compile(self, options: CompileState, **kwargs):
+        yield f"{self.type_} {self.name}"
+
+
+@dataclass
+class FunctionDef(BaseAst):
+    _fields = ("return_type", "name", "args", "modifier", "body")
+
+    return_type: str
+    name: str
+    args: List[Any]
+    modifier: str
+    body: List[Any]
+
+    @autoindent
+    def compile(self, options: CompileState, **kwargs):
+        if len(self.args) > 0:
+            yield f"{self.return_type} {self.name}("
+            for arg in self.args:
+                yield from arg.compile(options, **kwargs)
+            yield f") {self.modifier} {{"
+        else:
+            yield f"{self.return_type} {self.name}() {self.modifier} {{"
+
+        for component in self.body:
+            yield from component.compile(options, **kwargs)
+
+        yield "}"
+
+
+@dataclass
+class Return(BaseAst):
+    _fields = ("value",)
+
+    value: str
+
+    @autoindent
+    def compile(self, options: CompileState, **kwargs):
+        yield f"return {self.value};"
 
 
 StateOptions = ClassDef(
@@ -480,7 +557,64 @@ StateOptions = ClassDef(
     ],
 )
 
-namespace = Namespace(name="featuretest", body=[StateOptions])
+State = ClassDef(
+    "struct",
+    "State",
+    bases=[],
+    body=[
+        MemberDeclaration("static constexpr size_t", "rows", 9),
+        MemberDeclaration("static constexpr size_t", "cols", 1),
+        # TODO(buck): Eigen::Matrix<...> can be split into its own structure
+        UsingDeclaration("DataT", "Eigen::Matrix<double, rows, cols>"),
+        ConstructorDeclaration(),  # No args constructor gets default constructor
+        ConstructorDeclaration(Arg("const StateOptions&", "options")),
+    ]
+    + list(
+        chain.from_iterable(
+            [
+                (
+                    # TODO(buck): fill in the body
+                    FunctionDef(
+                        "double&",
+                        name,
+                        args=[],
+                        modifier="",
+                        body=[
+                            Return(f"data({idx}, 0)"),
+                        ],
+                    ),
+                    FunctionDef(
+                        "double",
+                        name,
+                        args=[],
+                        modifier="const",
+                        body=[
+                            Return(f"data({idx}, 0)"),
+                        ],
+                    ),
+                )
+                for idx, name in enumerate(
+                    [
+                        "CON_ori_pitch",
+                        "CON_ori_roll",
+                        "CON_ori_yaw",
+                        "CON_pos_pos_x",
+                        "CON_pos_pos_y",
+                        "CON_pos_pos_z",
+                        "CON_vel_x",
+                        "CON_vel_y",
+                        "CON_vel_z",
+                    ]
+                )
+            ]
+        )
+    )
+    + [
+        MemberDeclaration("DataT", "data", "DataT::Zero()"),
+    ],
+)
+
+namespace = Namespace(name="featuretest", body=[StateOptions, State])
 
 includes = [
     "#include <Eigen/Dense>    // Matrix",
