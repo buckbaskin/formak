b8c1a8854db9de69fe2072d6fb3c6c0b0eccee51
Buck Baskin <mobile.wbaskin@gmail.com>
2023-05-08 22:10:36 -0700

    WIP: Last 2 features need passing, one passed with quick work

--- featuretests/ast_code_generation/components_test.py
+++ featuretests/ast_code_generation/components_test.py
@@ -862,6 +862,154 @@ def test_classdef_altitudesensormodel():
 
 
 @gen_comp
+def test_forwarddeclaration_extendedkalmanfilterprocessmodel():
+    """
+    class ExtendedKalmanFilterProcessModel;
+    """
+    return ForwardClassDeclaration("class", "ExtendedKalmanFilterProcessModel")
+
+
+@gen_comp
+def test_classdef_extendedkalmanfilter():
+    """
+    class ExtendedKalmanFilter {
+     public:
+      using CovarianceT =
+          Eigen::Matrix<double, 6, 6>;
+      using ProcessJacobianT =
+          Eigen::Matrix<double, 9, 9>;
+      using ControlJacobianT =
+          Eigen::Matrix<double, 9, 6>;
+      using ProcessModel = ExtendedKalmanFilterProcessModel;
+
+      StateAndVariance process_model(
+          double dt,
+          const StateAndVariance& input
+          ,
+          const Calibration& input_calibration
+          ,
+          const Control& input_control
+      );
+
+      template <typename ReadingT>
+      StateAndVariance sensor_model(
+          const StateAndVariance& input,
+          const Calibration& input_calibration,
+          const ReadingT& input_reading) {
+        const State& state = input.state;                 // mu
+        const Covariance& covariance = input.covariance;  // Sigma
+
+        // z_est = sensor_model()
+        const ReadingT reading_est =
+            ReadingT::SensorModel::model(input,
+                                         input_calibration,
+                                         input_reading);  // z_est
+
+        // H = Jacobian(z_est w.r.t. state)
+        const typename ReadingT::SensorJacobianT H =
+            ReadingT::SensorModel::jacobian(input,
+                                            input_calibration,
+                                            input_reading);
+
+        // Project State Noise into Sensor Space
+        // S = H * Sigma * H.T + Q_t
+        const typename ReadingT::CovarianceT sensor_estimate_covariance =
+            H * covariance.data * H.transpose() +
+            ReadingT::SensorModel::covariance(input,
+                                              input_calibration,
+                                              input_reading);
+
+        // S_inv = inverse(S)
+        const typename ReadingT::CovarianceT S_inv =
+            sensor_estimate_covariance.inverse();
+
+        // Kalman Gain
+        // K = Sigma * H.T * S_inv
+        const typename ReadingT::KalmanGainT kalman_gain =
+            covariance.data * H.transpose() * S_inv;
+
+        // Innovation
+        // innovation = z - z_est
+        const typename ReadingT::InnovationT innovation =
+            input_reading.data - reading_est.data;
+        _innovations[ReadingT::Identifier] = innovation;
+
+        // Update State Estimate
+        // next_state = state + K * innovation
+        State next_state;
+        next_state.data = state.data + kalman_gain * innovation;
+
+        // Update Covariance
+        // next_covariance = Sigma - K * H * Sigma
+        Covariance next_covariance;
+        next_covariance.data =
+            covariance.data - kalman_gain * H * covariance.data;
+
+        // Here be the StateAndVariance math
+        return StateAndVariance{.state = next_state,
+                                .covariance = next_covariance};
+      }
+
+      template <typename ReadingT>
+      std::optional<typename ReadingT::InnovationT> innovations() {
+        if (_innovations.count(ReadingT::Identifier) > 0) {
+          return std::any_cast<typename ReadingT::InnovationT>(
+              _innovations[ReadingT::Identifier]);
+        }
+        return {};
+      }
+
+     private:
+      std::unordered_map<SensorId, std::any> _innovations;
+    };
+    """
+    return Escape("")
+
+
+@gen_comp
+def test_classdef_extendedkalmanfilterprocessmodel():
+    """
+    class ExtendedKalmanFilterProcessModel {
+     public:
+      static State model(
+          double dt,
+          const StateAndVariance& input
+          ,
+          const Calibration& input_calibration
+          ,
+          const Control& input_control
+      );
+
+      static typename ExtendedKalmanFilter::ProcessJacobianT process_jacobian(
+          double dt,
+          const StateAndVariance& input
+          ,
+          const Calibration& input_calibration
+          ,
+          const Control& input_control
+      );
+
+      static typename ExtendedKalmanFilter::ControlJacobianT control_jacobian(
+          double dt,
+          const StateAndVariance& input
+          ,
+          const Calibration& input_calibration
+          ,
+          const Control& input_control
+      );
+
+      static typename ExtendedKalmanFilter::CovarianceT covariance(
+          double dt,
+          const StateAndVariance& input,
+          const Calibration& input_calibration      ,
+          const Control& input_control
+      );
+    };
+    """
+    return Escape("")
+
+
+@gen_comp
 def test_namespace():
     """
     namespace featuretest {
