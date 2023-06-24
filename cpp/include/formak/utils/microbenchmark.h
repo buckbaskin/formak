a88f44b9aec732b748002855ba5adc717768f6ca
Buck Baskin <mobile.wbaskin@gmail.com>
2023-06-23 21:45:59 -0700

    Move C++ to microbenchmark tool

---
+++ cpp/include/formak/utils/microbenchmark.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <algorithm>  // sort
+#include <chrono>
+#include <iterator>  // back_inserter
+#include <vector>
+
+namespace formak {
+template <typename InputT, typename Func>
+std::vector<std::chrono::nanoseconds> microbenchmark(
+    Func&& lambda, const std::vector<InputT>& inputs) {
+  std::vector<std::chrono::nanoseconds> times;
+
+  for (const InputT& i : inputs) {
+    const auto start = std::chrono::steady_clock::now();
+    lambda(i);
+    const auto end = std::chrono::steady_clock::now();
+    times.push_back(end - start);
+  }
+  std::sort(times.begin(), times.end());
+
+  return times;
+}
+}  // namespace formak
--- featuretests/common_subexpression_elimination/simple_test.cpp
+++ featuretests/common_subexpression_elimination/simple_test.cpp
@@ -1,10 +1,11 @@
 #include <cse/cse-model.h>  // Generated
+#include <formak/utils/microbenchmark.h>
 #include <gtest/gtest.h>
 #include <no_cse/no-cse-model.h>  // Generated
 
-#include <algorithm>
 #include <chrono>
-#include <ostream>
+#include <iterator>  // back_inserter
+#include <ostream>   // cout
 #include <random>
 #include <tuple>
 #include <vector>
@@ -25,9 +26,7 @@ TEST(CppModel, Simple) {
   cse::Model cse_model;
   no_cse::Model no_cse_model;
 
-  no_cse_model.model(0.1, no_cse::StateOptions{.left = 1.0, .right = 0.1});
-
-  auto input = ([]() {
+  auto input_base = ([]() {
     size_t seed = 1;
     std::minstd_rand prng(seed);
     std::uniform_real_distribution<> dist(0, 1);
@@ -41,28 +40,44 @@ TEST(CppModel, Simple) {
     return result;
   })();
 
-  std::vector<std::chrono::nanoseconds> cse_times;
-  std::vector<std::chrono::nanoseconds> no_cse_times;
+  std::vector<cse::StateOptions> cse_input;
+  std::transform(input_base.cbegin(), input_base.cend(),
+                 std::back_inserter(cse_input),
+                 [](std::tuple<double, double> t) {
+                   const auto& [left, right] = t;
+                   return cse::StateOptions{
+                       .left = left,
+                       .right = right,
+                   };
+                 });
+  std::vector<no_cse::StateOptions> no_cse_input;
+  std::transform(input_base.cbegin(), input_base.cend(),
+                 std::back_inserter(no_cse_input),
+                 [](std::tuple<double, double> t) {
+                   const auto& [left, right] = t;
+                   return no_cse::StateOptions{
+                       .left = left,
+                       .right = right,
+                   };
+                 });
 
-  for (const auto& [left, right] : input) {
-    const auto cse_start = std::chrono::steady_clock::now();
-    cse_model.model(0.1, cse::StateOptions{.left = left, .right = right});
-    const auto cse_end = std::chrono::steady_clock::now();
-    cse_times.push_back(cse_end - cse_start);
-  }
-  std::cout << "cse time:" << std::endl << cse_times;
-  std::sort(cse_times.begin(), cse_times.end());
+  std::vector<std::chrono::nanoseconds> cse_times = formak::microbenchmark(
+      [&cse_model](const cse::StateOptions& options) {
+        cse_model.model(0.1, options);
+      },
+      cse_input);
+  std::vector<std::chrono::nanoseconds> no_cse_times = formak::microbenchmark(
+      [&no_cse_model](const no_cse::StateOptions& options) {
+        no_cse_model.model(0.1, options);
+      },
+      no_cse_input);
+
   double cse_p99_slowest = cse_times[98].count() / 1.0e6;
-
-  for (const auto& [left, right] : input) {
-    const auto no_cse_start = std::chrono::steady_clock::now();
-    no_cse_model.model(0.1, no_cse::StateOptions{.left = left, .right = right});
-    const auto no_cse_end = std::chrono::steady_clock::now();
-    no_cse_times.push_back(no_cse_end - no_cse_start);
-  }
-  std::cout << "no cse time:" << std::endl << no_cse_times;
-  std::sort(no_cse_times.begin(), no_cse_times.end());
   double no_cse_p01_fastest = no_cse_times[1].count() / 1.0e6;
+  std::cout << "CSE    " << cse_p99_slowest << std::endl
+            << cse_times << std::endl;
+  std::cout << "No CSE " << no_cse_p01_fastest << std::endl
+            << cse_times << std::endl;
 
   EXPECT_LT(cse_p99_slowest, no_cse_p01_fastest);
 }
