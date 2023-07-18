cdb5e7b890306b5197e6a25caf963c9a0791ff6c
Buck Baskin <mobile.wbaskin@gmail.com>
2023-07-17 22:38:34 -0700

    WIP: Revise Python feature testing

--- featuretests/managed_filter/tick_interface_test.py
+++ featuretests/managed_filter/tick_interface_test.py
@@ -1,15 +1,64 @@
 import numpy as np
 import pytest
 
-from formak import runtime
+from formak import python, runtime, ui
+
+
+def make_ekf():
+    dt = ui.Symbol("dt")
+
+    tp = {k: ui.Symbol(k) for k in ["mass", "z", "v", "a"]}
+
+    thrust = ui.Symbol("thrust")
+
+    state = set(tp.values())
+    control = {thrust}
+
+    state_model = {
+        tp["mass"]: tp["mass"],
+        tp["z"]: tp["z"] + dt * tp["v"],
+        tp["v"]: tp["v"] + dt * tp["a"],
+        tp["a"]: -9.81 * tp["mass"] + thrust,
+    }
+
+    model = ui.Model(dt=dt, state=state, control=control, state_model=state_model)
+
+    ekf = python.compile_ekf(
+        state_model=model,
+        process_noise={thrust: 1.0},
+        sensor_models={"simple": {ui.Symbol("v"): ui.Symbol("v")}},
+        sensor_noises={"simple": np.eye(1)},
+    )
+    return ekf
 
 
 def test_tick_time_only():
-    mf = runtime.ManagedFilter()
+    # featuretest::State state(featuretest::StateOptions{.v = 1.0});
+    # formak::runtime::ManagedFilter<featuretest::ExtendedKalmanFilter> mf(
+    #     0.0, {
+    #              .state = state,
+    #              .covariance = {},
+    #          });
 
-    state0p1 = mf.tick(0.1)
+    # featuretest::Control control;
 
-    state0p2 = mf.tick(0.2)
+    # auto state0p1 = mf.tick(0.1, control);
+
+    # auto state0p2 = mf.tick(0.2, control);
+
+    # EXPECT_NE(state0p1.state.data, state0p2.state.data);
+    ekf = make_ekf()
+    state = np.array([[0.0, 0.0, 0.0, 0.0]]).transpose()
+    covariance = np.eye(4)
+    mf = runtime.ManagedFilter(
+        ekf=ekf, start_time=0.0, state=state, covariance=covariance
+    )
+
+    control = np.array([[0.0]])
+
+    state0p1 = mf.tick(0.1, control)
+
+    state0p2 = mf.tick(0.2, control)
 
     assert state0p1 != state0p2
 
--- py/formak/runtime.py
+++ py/formak/runtime.py
@@ -1,7 +1,62 @@
-class ManagedFilter(object):
-    def tick(self, output_time, readings=None):
-        for sensor_reading in readings:
-            self.state = process_model(sensor_reading.timestamp, self.state)
-            self.state = sensor_model(sensor_reading, self.state)
+from collections import namedtuple
+from typing import List, Optional
 
-        return process_model(self.state)
+StampedReading = namedtuple("StampedReading", ["timestamp", "data"])
+
+
+class ManagedFilter(object):
+    def __init__(self, ekf, start_time: float, state, covariance):
+        self._impl = ekf
+        self.current_time = start_time
+        self.state = state
+        self.covariance = covariance
+
+    def tick(self, output_time: float, readings: Optional[List[StampedReading]] = None):
+        if readings is None:
+            readings = []
+
+        for sensor_reading in readings:
+            assert isinstance(sensor_reading, StampedReading)
+
+            self.current_time, (self.state, self.covariance) = self._process_model(
+                sensor_reading.timestamp, self.state, self.covariance
+            )
+
+            (self.state, self.covariance) = self._impl.sensor_model(
+                sensor_reading.data, self.state, self.covariance
+            )
+
+        return self._process_model(output_time, control)
+
+    def _process_model(self, target_time, state, covariance):
+        # const
+        raise NotImplementedError("_process_model")
+
+    # typename Impl::StateAndVarianceT tick(
+    #     double outputTime, const typename Impl::ControlT& control) {
+    #   return processUpdate(outputTime, control);
+    # }
+    # typename Impl::StateAndVarianceT tick(
+    #     double outputTime, const typename Impl::ControlT& control,
+    #     const std::vector<StampedReading>& readings) {
+    #   for (const auto& stampedReading : readings) {
+    #     _state = processUpdate(stampedReading.timestamp, control);
+    #     _currentTime = stampedReading.timestamp;
+
+    #     _state = stampedReading.data->sensor_model(_impl, _state);
+    #   }
+
+    #   return tick(outputTime, control);
+    # }
+
+    # private:
+    # typename Impl::StateAndVarianceT processUpdate(
+    #     double outputTime, const typename Impl::ControlT& control) const {
+    #   double dt = 0.1;
+    #   typename Impl::StateAndVarianceT state = _state;
+    #   for (double currentTime = _currentTime; currentTime < outputTime;
+    #        currentTime += dt) {
+    #     state = _impl.process_model(dt, state, control);
+    #   }
+    #   return state;
+    # }
