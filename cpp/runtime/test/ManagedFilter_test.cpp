afb7efec606312b2c83f0741fb5d4fceccbaa460
Buck Baskin <mobile.wbaskin@gmail.com>
2023-07-24 11:49:31 -0700

    Red: Dt not handled correctly by ManagedFilter

--- cpp/runtime/test/ManagedFilter_test.cpp
+++ cpp/runtime/test/ManagedFilter_test.cpp
@@ -1,17 +1,23 @@
 #include <formak/runtime/ManagedFilter.h>
 #include <gtest/gtest.h>
 
+#include <vector>
+
 namespace unit {
 
+namespace {
 struct StateAndVariance {
   double state = 0.0;
   double covariance = 1.0;
 };
 // TODO(buck): Do all filters have a control?
-struct Control {};
+struct Control {
+  double velocity = 0.0;
+};
 
 struct StampedReadingBase;
 
+// Note: ManagedFilter assumes the Impl can be default constructed
 struct TestImpl {
   using StateAndVarianceT = StateAndVariance;
   using ControlT = Control;
@@ -21,6 +27,14 @@ struct TestImpl {
   StateAndVariance sensor_model(const StateAndVarianceT& input,
                                 const ReadingT& reading) const {
     return input;
+  }
+
+  StateAndVariance process_model(double dt, const StateAndVariance& state,
+                                 const ControlT& control) const {
+    return {
+        .state = state.state + dt * control.velocity,
+        .covariance = state.covariance + dt,
+    };
   }
 };
 
@@ -41,39 +55,19 @@ struct Reading : public StampedReadingBase {
 
   double reading = 0.0;
 };
+}  // namespace
 
-TEST(ManagedFilter, Constructor) {
-  // template <typename Impl>
-  // class ManagedFilter {
-  //  public:
-  //   ManagedFilter(double initialTimestamp,
-  //                 const typename Impl::StateAndVarianceT& initialState)
-  //       : _currentTime(initialTimestamp), _state(initialState) {
-  //   }
-  // }
-
+TEST(ManagedFilterTest, Constructor) {
   // [[maybe_unused]] because this test is focused on the constructor only.
   // Passes if construction and deconstruction are successful
   [[maybe_unused]] formak::runtime::ManagedFilter<TestImpl> mf(
       1.23, StateAndVariance{.state = 4.0, .covariance = 1.0});
 
-  // Note(buck): This test is helpful (compiler error because _impl isn't
-  // initialized...)
+  // Note(buck): This test is helpful (found a compiler error because _impl
+  // wasn't initialized...)
 }
 
-// struct StampedReading {
-//   double timestamp = 0.0;
-//   std::shared_ptr<typename Impl::StampedReadingBaseT> data;
-// };
-// template <typename ReadingT>
-// StampedReading wrap(double timestamp, const ReadingT& reading) const {
-//   return StampedReading{
-//       .timestamp = timestamp,
-//       .data = std::shared_ptr<typename Impl::StampedReadingBaseT>(
-//           new ReadingT(reading)),
-//   };
-// }
-TEST(ManagedFilter, StampedReading) {
+TEST(ManagedFilterTest, StampedReading) {
   using formak::runtime::ManagedFilter;
 
   double reading = 1.0;
@@ -85,15 +79,55 @@ TEST(ManagedFilter, StampedReading) {
   StateAndVariance state;
 
   EXPECT_DOUBLE_EQ(stamped_reading.timestamp, 5.0);
+  // Can't directly address the .reading member of the child type. Instead, use
+  // the sensor_model interface to access (by stuffing into the .state member of
+  // the output)
   EXPECT_DOUBLE_EQ(stamped_reading.data->sensor_model(impl, state).state,
                    reading);
 }
 
-//
 // typename Impl::StateAndVarianceT tick(
 //     double outputTime, const typename Impl::ControlT& control) {
 //   return processUpdate(outputTime, control);
 // }
+namespace tick {
+struct Options {
+  double dt;
+};
+std::ostream& operator<<(std::ostream& o, const Options& options) {
+  o << "Options{.dt=" << options.dt << "}";
+  return o;
+}
+class ManagedFilterTest : public ::testing::Test,
+                          public ::testing::WithParamInterface<Options> {};
+
+TEST_P(ManagedFilterTest, TickNoReadings) {
+  using formak::runtime::ManagedFilter;
+
+  double start_time = 10.0;
+  StateAndVariance initial_state{
+      .state = 4.0,
+      .covariance = 1.0,
+  };
+  ManagedFilter<TestImpl> mf(start_time, initial_state);
+
+  Control control{.velocity = -1.0};
+
+  double dt = GetParam().dt;
+  StateAndVariance next_state = mf.tick(start_time + dt, control);
+
+  EXPECT_DOUBLE_EQ(next_state.state, 4.0 + dt * control.velocity);
+  if (GetParam().dt != 0.0) {
+    EXPECT_GT(next_state.covariance, initial_state.covariance);
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(TickTimings, ManagedFilterTest,
+                         ::testing::Values(Options{-1.5}, Options{-0.1},
+                                           Options{0.0}, Options{0.1},
+                                           Options{2.7}));
+}  // namespace tick
+
 // typename Impl::StateAndVarianceT tick(
 //     double outputTime, const typename Impl::ControlT& control,
 //     const std::vector<StampedReading>& readings) {
