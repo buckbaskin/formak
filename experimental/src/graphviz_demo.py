38fac57c0e990b80cad095e43392445e907cb16d
Buck Baskin <mobile.wbaskin@gmail.com>
2023-08-12 00:49:53 -0700

    V1 looks nice

--- experimental/src/graphviz_demo.py
+++ experimental/src/graphviz_demo.py
@@ -9,41 +9,51 @@ def make_expr():
     expr_v2 = expand_trig(expr_v1)
     return expr_v1, expr_v2
 
+def render_expr(expr):
+    if len(expr.args) == 0:
+        if expr.func == sympy.core.numbers.One:
+            return 'One'
+        elif expr.func == sympy.core.numbers.Zero:
+            return 'Zero'
+        elif expr.func == sympy.core.numbers.NegativeOne:
+            return '-One'
+        elif expr.func == sympy.core.numbers.Integer:
+            return str(expr)
+        elif expr.func == sympy.core.symbol.Symbol:
+            return str(expr)
+        else:
+            raise ValueError('what do unknown? %s' % (expr,))
+
+    else:
+        if expr.func == sympy.core.add.Add:
+            return '+'
+        elif expr.func == sympy.core.mul.Mul:
+            return '*'
+        elif isinstance(expr.func, sympy.core.function.FunctionClass):
+            return str(expr.func)
+        else:
+            raise ValueError(f'what do with args? expr {expr}, type {type(expr.func)} func {expr.func}')
+
+
 def enumerate_expression(expr, root=None):
     if root is None:
         print('enumerating:', expr)
         root = deque()
 
-    if len(expr.args) == 0:
-        if expr.func == sympy.core.numbers.One:
-            root.append('One')
-        elif expr.func == sympy.core.numbers.Zero:
-            root.append('Zero')
-        elif expr.func == sympy.core.numbers.NegativeOne:
-            root.append('-One')
-        elif expr.func == sympy.core.numbers.Integer:
-            root.append(str(expr))
-        elif expr.func == sympy.core.symbol.Symbol:
-            root.append(str(expr))
-        else:
-            raise ValueError('what do unknown? %s' % (expr,))
+    child_ids = []
 
-    else:
-        print(expr.func, expr.args)
-        if expr.func == sympy.core.add.Add:
-            root.append('+')
-        elif expr.func == sympy.core.mul.Mul:
-            root.append('*')
-        elif isinstance(expr.func, sympy.core.function.FunctionClass):
-            root.append(str(expr.func))
-        else:
-            raise ValueError(f'what do with args? expr {expr}, type {type(expr.func)} func {expr.func}')
-
-    id_ = 'root|' + '|'.join(root)
-    yield id_, expr
+    root.append(render_expr(expr))
 
     for child in expr.args:
-        yield from enumerate_expression(child, root)
+        for child_id, child_expr, child_children in enumerate_expression(child, root):
+            yield child_id, child_expr, child_children
+            if child_expr == child:
+                # This is false when yielding a grand-child (child's child)
+                child_ids.append(child_id)
+
+    id_ = 'root|' + '|'.join(root)
+    yield id_, expr, child_ids
+
 
     root.pop()
 
@@ -51,14 +61,12 @@ def main():
 
     v1, v2 = make_expr()
 
-    for id_, expr in enumerate_expression(v1):
-        print(id_)
+    dot = Digraph(comment='Compute Graph')
 
-    dot = graphviz.Digraph(comment='The Round Table')
-    dot.node('A', 'King Arthur')
-    dot.node('B', 'Sir Bedevere the Wise')
-    dot.node('L', 'Sir Lancelot the Brave')
-    dot.edge('B', 'L', constraint='true')
+    for id_, expr, child_ids in enumerate_expression(v1):
+        dot.node(id_, render_expr(expr))
+        for child_id in child_ids:
+            dot.edge(id_, child_id, constraint='true')
 
     dot.render(view=True)
 
