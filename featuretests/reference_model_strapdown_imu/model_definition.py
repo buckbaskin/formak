1a60c9012b0597ebe62245689a9493835e242ec1
Buck Baskin <mobile.wbaskin@gmail.com>
2023-09-22 21:11:44 -0700

    Nominal first version of the model

--- featuretests/reference_model_strapdown_imu/model_definition.py
+++ featuretests/reference_model_strapdown_imu/model_definition.py
@@ -1,11 +1,15 @@
-from sympy import cos, sec, sin, tan
+from sympy import Pow, cos, integrate, sec, simplify, sin, tan
 
 from formak import ui
 
 dt = ui.Symbol("dt")
 
-imu_gyro = ui.symbols(["w_1", "w_2", "w_3"])
-imu_accel = ui.symbols(["f_1", "f_2", "f_3"])
+
+def axis_set(name):
+    return ui.symbols([f"{name}_{{1}}", f"{name}_{{2}}", f"{name}_{{3}}"])
+
+
+imu_gyro = axis_set("\omega")
 
 yaw, pitch, roll = ui.symbols([r"\psi", r"\theta", r"\phi"])
 yaw_rate, pitch_rate, roll_rate = ui.symbols(
@@ -20,20 +24,78 @@ gyro_rotations = ui.Matrix(
     ]
 )
 
-body_rates = gyro_rotations * ui.Matrix(imu_gyro)
+gyro_body_rates = gyro_rotations * ui.Matrix(imu_gyro)
 
-state = set([yaw, pitch, roll, yaw_rate, pitch_rate, roll_rate])
+assert gyro_body_rates.shape == (3, 1)
 
-assert body_rates.shape == (3, 1)
+imu_accel = axis_set("f")
 
+global_pose = axis_set("x_{A}")
+global_velocity = axis_set("\dot{x}_{A}")
+global_accel = axis_set("\ddot{x}_{A}")
+g = ui.Symbol("g")  # gravity
+
+accel_gravity = ui.Matrix([0, 0, -g])
+assert accel_gravity.shape == (3, 1)
+
+yaw_rotation = ui.Matrix(
+    [
+        [cos(yaw), -sin(yaw), 0],
+        [sin(yaw), cos(yaw), 0],
+        [0, 0, 1],
+    ]
+)
+pitch_rotation = ui.Matrix(
+    [
+        [cos(pitch), 0, sin(pitch)],
+        [0, 1, 0],
+        [-sin(pitch), 0, cos(pitch)],
+    ]
+)
+roll_rotation = ui.Matrix(
+    [
+        [1, 0, 0],
+        [0, cos(roll), -sin(roll)],
+        [0, sin(roll), cos(roll)],
+    ]
+)
+
+accel_body_rates = (
+    yaw_rotation * pitch_rotation * roll_rotation * ui.Matrix(imu_accel) + accel_gravity
+)
+assert accel_body_rates.shape == (3, 1)
+
+state = set(
+    [yaw, pitch, roll, yaw_rate, pitch_rate, roll_rate]
+    + global_pose
+    + global_velocity
+    + global_accel
+)
 state_model = {
-    yaw: yaw + dt * yaw_rate,
-    yaw_rate: body_rates[0, 0],
-    pitch: pitch + dt * pitch_rate,
-    pitch_rate: body_rates[1, 0],
-    roll: roll + dt * roll_rate,
-    roll_rate: body_rates[2, 0],
+    # Rotation
+    yaw_rate: gyro_body_rates[0, 0],
+    pitch_rate: gyro_body_rates[1, 0],
+    roll_rate: gyro_body_rates[2, 0],
+    # Rotation Integration
+    yaw: yaw + integrate(yaw_rate, dt),
+    pitch: pitch + integrate(pitch_rate, dt),
+    roll: roll + integrate(roll_rate, dt),
+    # Translation
+    global_accel[0]: accel_body_rates[0, 0],
+    global_accel[1]: accel_body_rates[1, 0],
+    global_accel[2]: accel_body_rates[2, 0],
+    # Translation Integration
+    global_velocity[0]: global_velocity[0] + integrate(global_accel[0], dt),
+    global_velocity[1]: global_velocity[1] + integrate(global_accel[1], dt),
+    global_velocity[2]: global_velocity[2] + integrate(global_accel[2], dt),
+    global_pose[0]: global_pose[0]
+    + integrate(global_velocity[0] + integrate(global_accel[0], dt), dt),
+    global_pose[1]: global_pose[1]
+    + integrate(global_velocity[1] + integrate(global_accel[1], dt), dt),
+    global_pose[2]: global_pose[2]
+    + integrate(global_velocity[2] + integrate(global_accel[2], dt), dt),
 }
+
 
 if __name__ == "__main__":
     print(gyro_rotations.shape)
