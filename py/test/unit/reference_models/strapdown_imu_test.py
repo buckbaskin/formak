80e3959df1570b76f7034216ed70d64dedab0a7e
Buck Baskin <mobile.wbaskin@gmail.com>
2023-10-26 08:46:26 -0700

    Add some extra testing for model

--- py/test/unit/reference_models/strapdown_imu_test.py
+++ py/test/unit/reference_models/strapdown_imu_test.py
@@ -96,36 +96,6 @@ def test_stationary():
     assert np.allclose(state.data, expected_state.data)
 
 
-def plot_timeseries(*, times, states, expected_states, arglist, x_name, file_id):
-    x = ui.Symbol(x_name)
-
-    plt.plot(
-        times,
-        states[:, arglist.index(x)],
-        label="model",
-    )
-    plt.plot(
-        times,
-        expected_states[:, arglist.index(x)],
-        label="reference",
-        alpha=0.5,
-    )
-    plt.scatter(
-        times,
-        states[:, arglist.index(x)],
-        label="model",
-    )
-    plt.scatter(
-        times,
-        expected_states[:, arglist.index(x)],
-        label="reference",
-        alpha=0.5,
-    )
-    plt.legend()
-    plt.savefig(f"{file_id}.png")
-    print(f"Write Timeseries {file_id}.png")
-
-
 def test_circular_motion_xy_plane():
     print("state", sorted(list(strapdown_imu.state), key=lambda s: str(s)))
     print("control", sorted(list(strapdown_imu.control), key=lambda s: str(s)))
@@ -304,3 +274,98 @@ def test_circular_motion_xy_plane():
     print("Write image")
 
     assert np.allclose(state.data, expected_state.data, atol=ALLOWED_TOL)
+
+def test_circular_motion_xz_plane():
+    print("state", sorted(list(strapdown_imu.state), key=lambda s: str(s)))
+    print("control", sorted(list(strapdown_imu.control), key=lambda s: str(s)))
+
+    print("state_model")
+    for k in sorted(list(strapdown_imu.state_model.keys()), key=lambda k: str(k)):
+        v = strapdown_imu.state_model[k]
+        print("key", k, "value", v)
+    imu = python.compile(
+        symbolic_model=strapdown_imu.symbolic_model,
+        calibration_map={
+            strapdown_imu.g: -9.81,
+            strapdown_imu.coriw: 1.0,
+            strapdown_imu.corix: 0.0,
+            strapdown_imu.coriy: 0.0,
+            strapdown_imu.coriz: 0.0,
+            strapdown_imu.accel_sensor_bias[0]: 0.0,
+            strapdown_imu.accel_sensor_bias[1]: 0.0,
+            strapdown_imu.accel_sensor_bias[2]: 0.0,
+        },
+    )
+    assert imu is not None
+    imu_gyro = strapdown_imu.imu_gyro
+    imu_accel = strapdown_imu.imu_accel
+
+    rate = 100  # Hz
+    dt = 1.0 / rate
+
+    # Circular Motion on X, Y plane rotating around Z axis
+    radius = 5.0
+    yaw_rate = pi
+
+    # travel = r * \psi
+    velocity = radius * yaw_rate
+    specific_force = radius * yaw_rate * yaw_rate
+
+    orientation = Quaternion.from_euler([0.0, -radians(90), 0.0], "zyx")
+    state = imu.State.from_dict(
+        {
+            r"\ddot{x}_{A}_{1}": -specific_force,
+            r"\dot{\psi}": yaw_rate,
+            r"\dot{x}_{A}_{3}": velocity,
+            "oriw": orientation.a,
+            "orix": orientation.b,
+            "oriy": orientation.c,
+            "oriz": orientation.d,
+            r"x_{A}_{1}": radius,
+            r"x_{A}_{3}": 0.0,
+        }
+    )
+
+    control_args = {imu_gyro[1]: -pi, imu_accel[0]: -9.81, imu_accel[2]: -specific_force}
+    control = imu.Control.from_dict(control_args)
+
+    print("dt", dt)
+    print("state 0", state, state._arglist, "\n", state.data)
+    print("control", control, control._arglist, "\n", control.data)
+
+    times = [0.0]
+    states = [state.data]
+    expected_states = [state.data]
+
+    ALLOWED_TOL = 0.5
+
+    # Check Initial State
+    state = imu.model(0.0, state, control)
+    expected_pitch = -radians(90)
+    expected_orientation = Quaternion.from_axis_angle([0.0, 1.0, 0.0], expected_pitch)
+    expected_state = imu.State.from_dict(
+        {
+            r"\ddot{x}_{A}_{1}": -specific_force * sin(expected_pitch),
+            r"\ddot{x}_{A}_{2}": 0.0,
+            r"\ddot{x}_{A}_{3}": specific_force * cos(expected_pitch),
+            r"\dot{\phi}": 0.0,
+            r"\dot{\psi}": 0.0,
+            r"\dot{\theta}": -yaw_rate,
+            r"\dot{x}_{A}_{1}": velocity * cos(expected_pitch),
+            r"\dot{x}_{A}_{2}": 0.0,
+            r"\dot{x}_{A}_{3}": -velocity * sin(expected_pitch),
+            "oriw": expected_orientation.a,
+            "orix": expected_orientation.b,
+            "oriy": expected_orientation.c,
+            "oriz": expected_orientation.d,
+            r"x_{A}_{1}": radius * -sin(expected_pitch - yaw_rate * dt),
+            r"x_{A}_{2}": 0.0,
+            r"x_{A}_{3}": radius * -cos(expected_pitch - yaw_rate * dt) + velocity * dt,
+        }
+    )
+    if not np.allclose(state.data, expected_state.data, atol=ALLOWED_TOL):
+        print("Diff at index", 0)
+        state.render_diff(expected_state)
+        assert np.allclose(state.data, expected_state.data, atol=ALLOWED_TOL)
+
+    # This could be extended to vary the gravity effect as the model rotates...
