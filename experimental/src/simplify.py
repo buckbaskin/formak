7bae977b9eef56dcb61326ffdd344ebaf8308477
Buck Baskin <mobile.wbaskin@gmail.com>
2023-10-31 08:22:58 -0700

    Print out the various expressions to see what's going on

--- experimental/src/simplify.py
+++ experimental/src/simplify.py
@@ -1,30 +1,21 @@
 from datetime import datetime
 from sympy import Symbol, Quaternion, simplify, Matrix, symbols
 
-reference = [Symbol(k) for k in ["w", "x", "y", "z"]]
-
-r = Quaternion(*reference)
-
-r_mat = r.to_rotation_matrix()
-
-
-print("Start")
-
-start = datetime.now()
-
-result = simplify(r_mat)
-
-end = datetime.now()
-
-print('Runtime', end - start)
-
-reference = Matrix([symbols(['a', 'b', 'c']), symbols(['d', 'e', 'f']), symbols(['g', 'h', 'i'])])
+reference = Matrix(
+    [symbols(["a", "b", "c"]), symbols(["d", "e", "f"]), symbols(["g", "h", "i"])]
+)
 
 r = Quaternion.from_rotation_matrix(reference)
 
 r_mat = r.to_rotation_matrix()
 
+for i in range(3):
+    for j in range(3):
+        print(i, j, "\n", r_mat[i, j])
+
 print("Start 2")
+
+1 / 0
 
 start = datetime.now()
 
@@ -32,4 +23,4 @@ result = simplify(r_mat)
 
 end = datetime.now()
 
-print('Runtime 2', end - start)
+print("Runtime 2", end - start)
