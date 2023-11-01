fc45c01cf48f255df524604e663182de63d772c2
Buck Baskin <mobile.wbaskin@gmail.com>
2023-10-31 22:01:47 -0700

    First bottoms up results

--- experimental/src/simplify.py
+++ experimental/src/simplify.py
@@ -1,26 +1,68 @@
+import numpy as np
+from collections import defaultdict
 from datetime import datetime
-from sympy import Symbol, Quaternion, simplify, Matrix, symbols
-
-reference = Matrix(
-    [symbols(["a", "b", "c"]), symbols(["d", "e", "f"]), symbols(["g", "h", "i"])]
+from sympy import (
+    Symbol,
+    Quaternion,
+    simplify,
+    Matrix,
+    symbols,
+    Expr,
 )
 
-r = Quaternion.from_rotation_matrix(reference)
 
-r_mat = r.to_rotation_matrix()
+def decomposing_print(expr) -> None:
+    print({"func": expr.func, "args": expr.args})
 
-for i in range(3):
-    for j in range(3):
-        print(i, j, "\n", r_mat[i, j])
 
-print("Start 2")
+def bottoms_up_traversal(expr, level=0) -> None:
+    # print('Descending', level, expr.func)
 
-1 / 0
+    for arg in expr.args:
+        yield from bottoms_up_traversal(arg, level - 1)
 
-start = datetime.now()
+    yield expr
+    # print('Ascending', level, expr.func)
 
-result = simplify(r_mat)
 
-end = datetime.now()
+def main():
+    reference = Matrix(
+        [symbols(["a", "b", "c"]), symbols(["d", "e", "f"]), symbols(["g", "h", "i"])]
+    )
 
-print("Runtime 2", end - start)
+    r = Quaternion.from_rotation_matrix(reference)
+
+    r_mat = r.to_rotation_matrix()
+
+    # for i in range(3):
+    #     for j in range(3):
+    #         print(i, j, "\n", r_mat[i, j])
+
+    time_history = defaultdict(list)
+
+    for idx, expr in enumerate(bottoms_up_traversal(r_mat[0, 0])):
+        start = datetime.now()
+
+        simplify(expr)
+
+        end = datetime.now()
+
+        simplify_time = (end - start).total_seconds()
+        assert simplify_time >= 0.0
+        time_history[expr].append(simplify_time)
+
+        print("Index", idx, "\n", simplify_time)
+
+        if simplify_time > 1.0:
+            print("Big Expression\n", expr)
+
+        if simplify_time > 10.0:
+            print("Dump Timing Summary")
+            for key, times in sorted(list(time_history.items()), key=lambda t: (np.max(t[1]), str(t[0]))):
+                print(key, "|")
+                print("    |", len(times), np.average(times), np.max(times))
+            raise ValueError("Long Simplify")
+
+
+if __name__ == "__main__":
+    main()
