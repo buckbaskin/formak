bc7e4d900884c6bf7dd26da11e5597ddcd4c9eaf
Buck Baskin <mobile.wbaskin@gmail.com>
2023-12-07 00:57:08 -0800

    WIP: Implement TODO complicating the model with a calibration

--- featuretests/hyperparameter_tuning/model.py
+++ featuretests/hyperparameter_tuning/model.py
@@ -2,24 +2,29 @@ from formak import ui
 
 dt = ui.Symbol("dt")
 
-tp = _trajectory_properties = {k: ui.Symbol(k) for k in ["mass", "z", "v", "a"]}
+tp = _trajectory_properties = {k: ui.Symbol(k) for k in ["z", "v", "a"]}
 
 thrust = ui.Symbol("thrust")
+mass = ui.Symbol("mass")
 
 state = set(tp.values())
 control = {thrust}
 
 state_model = {
-    tp["mass"]: tp["mass"],
     tp["z"]: tp["z"] + dt * tp["v"],
     tp["v"]: tp["v"] + dt * tp["a"],
-    tp["a"]: -9.81 * tp["mass"] + thrust,
+    tp["a"]: -9.81 * mass + thrust,
 }
 
-symbolic_model = ui.Model(dt=dt, state=state, control=control, state_model=state_model)
+symbolic_model = ui.Model(
+    dt=dt,
+    state=state,
+    control=control,
+    state_model=state_model,
+    calibration=set([mass]),
+)
 
 process_noise = {thrust: 0.01}
 sensor_models = {"velocity": {tp["v"]: tp["v"]}}
 sensor_noises = {"velocity": {tp["v"]: 1.0}}
-# TODO add a calibration term here (mass?)
-calibration_map = {}  # type: Dict[str, float]
+calibration_map = {mass: 0.0}
