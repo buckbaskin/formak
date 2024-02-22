3a5ef7c64a34a830cdf0e067be4ce5999caae227
Buck Baskin <mobile.wbaskin@gmail.com>
2024-02-21 20:31:50 -0800

    Start to layout an A* example

--- experimental/test/superoptimizer_test.py
+++ experimental/test/superoptimizer_test.py
@@ -1,8 +1,51 @@
 from superoptimizer import superoptimizer, astar
+from collections import namedtuple
+
+# A -> B
+# |      \
+# C ----> D
+
+GraphEdge = namedtuple('GraphEdge', ['to_node', 'distance',])
+
+graph = {
+    'A': [GraphEdge('B', 1), GraphEdge('C', 1, ),],
+    'B': [GraphEdge('D', 1),],
+    'C': [GraphEdge('D', 3),],
+    'D': [],
+        }
+
+heuristic_map = {
+        'A': 0.0,
+        'B': 0.5,
+        'C': 0.0,
+        'D': 0.0,
+        }
 
 
 def test_astar():
-    result = astar()
+    class State(StateBase):
+        def __init__(self, name):
+            self.name = name
+
+    class Action(ActionBase):
+        def __init__(self, from_, to):
+            self.from_ = from_
+            self.to = to
+
+    def heuristic(state):
+        return heuristic_map[state.name]
+
+    def cost(action):
+        for edge in graph[action.from_]:
+            if edge.to_node == action.to:
+                return edge.distance
+
+        raise ValueError('No matching edge in graph')
+
+    def available_edges(state):
+        return graph[state.name]
+
+    result = astar(initial_state = State('A'), heuristic=heuristic, cost=cost, available_edges=available_edges)
     assert result == ["a", "b"]
 
 
