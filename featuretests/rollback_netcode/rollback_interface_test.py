40529e2b908e3128a375ec1a1e7263aac648cf8d
Buck Baskin <mobile.wbaskin@gmail.com>
2024-03-01 20:09:28 -0800

    Start outlining feature tests

---
+++ featuretests/rollback_netcode/rollback_interface_test.py
@@ -0,0 +1,11 @@
+"""
+Feature Test.
+
+Run the rollback interface with some simple examples.
+
+Passes if the rollback updates as expected the state
+"""
+
+
+def test_rollback_empty():
+    assert False
