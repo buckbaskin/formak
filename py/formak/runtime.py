c12df3c5f576d8f23a9776f05292d79315ccad8e
Buck Baskin <mobile.wbaskin@gmail.com>
2024-03-02 18:12:21 -0800

    Remove some debugging

--- py/formak/runtime.py
+++ py/formak/runtime.py
@@ -134,7 +134,6 @@ class Storage:
             covariance=covariance,
             sensors=list(sensors),
         )
-        print(' - store', {'time': time, 'row_time': row.time, 'queue': row.state.queue if row.state is not None else None, 'sensors': [s.sensor_key for s in row.sensors]})
 
         if len(self.data) > 0:
             # compare to last element if you would insert at the end of the list
@@ -178,7 +177,6 @@ class Storage:
         # TODO: this might need to scan forwards or backwards to get a state or a time with a state, covariance?
         retrieval_index = bisect_left(self.data, time, key=lambda e: e.time) - 1
         retrieval_index = max(0, min(retrieval_index, len(self.data) - 1))
-        print(' - load ', {'time': time, 'state_time': self.data[retrieval_index].state.time, 'queue': self.data[retrieval_index].state.queue})
         return self.data[retrieval_index]
 
     def scan(self, start_time=None, end_time=None):
