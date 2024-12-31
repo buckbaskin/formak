98c0c19ae0c1587a72950dcb1d1e6d28c9f7b40a
Buck Baskin <mobile.wbaskin@gmail.com>
2024-12-30 22:46:13 -0800

    TIL: you can only have one parent formak folder before the names become unfindable

--- py/formak/ui.py
+++ py/formak/ui.py
@@ -1,3 +1,16 @@
-from formak.ui_model import Model
+try:
+    from formak.problemdefinition.model import Model
+except ImportError:
+    print("womp womp what do")
+    import sys
+    prev_line = ''
+    for line in sys.path:
+        print(line[-80:])
+        prev_line = line
+
+    print('')
+    print('end')
+    raise
+
 from formak.ui_state_machine import DesignManager, NisScore, StateId
 from sympy import Matrix, Symbol, simplify, symbols
