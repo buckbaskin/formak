a5b26d13de6663280ea6ae09e60342a1fc69accd
Buck Baskin <mobile.wbaskin@gmail.com>
2025-01-07 21:45:08 -0800

    Split BasicBlock out of cpp

--- backend_cpp/backend_cpp/cpp.py
+++ backend_cpp/backend_cpp/cpp.py
@@ -1,9 +1,7 @@
 import argparse
 import logging
 from collections import namedtuple
-from dataclasses import dataclass
-from itertools import count
-from typing import Any, Iterable, List, Optional, Tuple
+from typing import Iterable
 
 from backend_cpp import ast_fragments as fragments
 from backend_cpp.ast import (
@@ -16,61 +14,18 @@ from backend_cpp.ast import (
     Return,
     SourceFile,
 )
+from backend_cpp.basic_block import BasicBlock
 from backend_cpp.compile_result import CppCompileResult
 from backend_cpp.config import Config
 from backend_py.named_covariance import named_covariance
 from backend_py.named_vector import named_vector
 from formak.exceptions import ModelConstructionError
 from frontend.model_validation import model_validation
-from sympy import Symbol, ccode, cse, diff, simplify
+from sympy import Symbol, diff
 
 DEFAULT_MODULES = ("scipy", "numpy", "math")
 
 logger = logging.getLogger(__name__)
-
-
-class BasicBlock:
-    """
-    A run of statements without control flow.
-
-    All statements can be reordered or changed to improve performance.
-    """
-
-    def __init__(
-        self, *, statements: List[Tuple[str, Any]], indent: int, config: Config
-    ):
-        # should be Tuple[str, sympy expression]
-        statements = list(statements)
-        self._targets = [k for k, _ in statements]
-        self._exprs = [v for _, v in statements]
-        self._indent = indent
-        self._config = config
-
-    def __len__(self):
-        return len(self._exprs)
-
-    def compile(self):
-        prefix = []
-        body = self._exprs
-
-        if self._config.common_subexpression_elimination:
-            prefix, body = cse(body, symbols=(Symbol(f"_t{i}") for i in count()))
-
-        # Note: The list of statements is ordered and can get CSE or reordered
-        # within the block because we know it is straight calculation without
-        # control flow (a basic block)
-        for target, expr in prefix:
-            assert isinstance(target, Symbol)
-            if self._config.common_subexpression_elimination:
-                expr = simplify(expr)
-            cc_expr = ccode(expr)
-            yield MemberDeclaration("double", target, cc_expr)
-
-        for target, expr in zip(self._targets, body):
-            if self._config.common_subexpression_elimination:
-                expr = simplify(expr)
-            cc_expr = ccode(expr)
-            yield MemberDeclaration("", target, cc_expr)
 
 
 class Model:
