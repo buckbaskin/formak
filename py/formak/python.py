1b141e97329fa769f94fe29214e2074bedb05c06
Buck Baskin <mobile.wbaskin@gmail.com>
2025-01-18 22:25:14 -0800

    Clean out external deps from python.py

--- py/formak/python.py
+++ py/formak/python.py
@@ -1,14 +1,3 @@
-from __future__ import annotations
-
-import dataclasses
-from typing import Any, Iterator
-
-import numpy as np
-from numpy.typing import NDArray
-from scipy.optimize import minimize
-from sklearn.base import BaseEstimator
-
-import sympy
 from backend_py.compile_ekf import compile_ekf
 from backend_py.config import Config
 from backend_py.extended_kalman_filter import (
@@ -20,4 +9,3 @@ from backend_py.named_vector import named_vector
 from backend_py.sensor_model import SensorModel
 from formak.exceptions import MinimizationFailure, ModelConstructionError
 from problemdefinition.ui_model_base import UiModelBase
-from sympy import Symbol
