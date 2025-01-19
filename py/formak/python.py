3554eb36058ff9c17ecde9559f7f594d126315f2
Buck Baskin <mobile.wbaskin@gmail.com>
2025-01-18 20:14:13 -0800

    Move BasicBlock out of python.py

--- py/formak/python.py
+++ py/formak/python.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import dataclasses
 from collections import namedtuple
-from itertools import count
 from math import sqrt
 from typing import Any, Iterator
 
@@ -12,81 +11,14 @@ from scipy.optimize import minimize
 from sklearn.base import BaseEstimator
 
 import sympy
+from backend_py.basic_block import BasicBlock
 from backend_py.config import Config
 from backend_py.named_covariance import named_covariance
 from backend_py.named_vector import named_vector
 from formak.exceptions import MinimizationFailure, ModelConstructionError
 from frontend.model_validation import model_validation
 from problemdefinition.ui_model_base import UiModelBase
-from sympy import Matrix, Symbol, cse, simplify
-from sympy.utilities.lambdify import lambdify
-
-
-class BasicBlock:
-    """
-    A run of statements without control flow.
-
-    All statements can be reordered or changed to improve performance.
-    """
-
-    def __init__(self, *, arglist: list[str], statements: list[Any], config: Config):
-        self._arglist = arglist
-        self._exprs = statements
-        self._config = config
-
-        self._compile()
-
-    def __len__(self):
-        return len(self._exprs)
-
-    def _compile(self):
-        prefix = []
-        body = self._exprs
-
-        if self._config.common_subexpression_elimination:
-            prefix, body = cse(body, symbols=(Symbol(f"_t{i}") for i in count()))
-
-        temporaries = [r[0] for r in prefix]
-        self._prefix = []
-        for i in range(len(prefix)):
-            expr = prefix[i][1]
-            if self._config.common_subexpression_elimination:
-                expr = simplify(expr)
-
-            self._prefix.append(
-                (
-                    temporaries[i],
-                    lambdify(
-                        self._arglist + temporaries[:i],
-                        expr,
-                        modules=self._config.python_modules,
-                        cse=False,
-                    ),
-                )
-            )
-
-        self._body = [
-            lambdify(
-                self._arglist + temporaries,
-                (
-                    simplify(expr)
-                    if self._config.common_subexpression_elimination
-                    else expr
-                ),
-                modules=self._config.python_modules,
-                cse=False,
-            )
-            for expr in body
-        ]
-
-    def execute(self, *args, **kwargs):
-        # Note: The list of statements is ordered and can get CSE or reordered within the block because we know it is straight calculation without control flow (a basic block)
-        temporary_values = {}
-        for name, expr in self._prefix:
-            temporary_values[str(name)] = expr(*args, **kwargs, **temporary_values)
-
-        for impl in self._body:
-            yield impl(*args, **kwargs, **temporary_values)
+from sympy import Matrix, Symbol
 
 
 class Model:
