13d5b5173c7ccc3cfb37bc9fb770d62681a898fc
Buck Baskin <mobile.wbaskin@gmail.com>
2025-01-18 22:32:29 -0800

    Remove unused (indirect) python.py imports

--- py/formak/python.py
+++ py/formak/python.py
@@ -1,11 +1,7 @@
 from backend_py.compile_ekf import compile_ekf
-from backend_py.config import Config
 from backend_py.extended_kalman_filter import (
     ExtendedKalmanFilter,
     assert_valid_covariance,
     nearest_positive_definite,
 )
-from backend_py.named_vector import named_vector
 from backend_py.sensor_model import SensorModel
-from formak.exceptions import MinimizationFailure, ModelConstructionError
-from problemdefinition.ui_model_base import UiModelBase
